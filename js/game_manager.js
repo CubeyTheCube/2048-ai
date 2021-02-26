03d2b596aa6085a2dcc3633121d7b833ea075f7e
CubeyTheCube <72284516+CubeyTheCube@users.noreply.github.com>
2021-02-26 14:55:11 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -87,7 +87,7 @@ GameManager.prototype.setup = function () {
 
 var globalGameManager = null;
 
-var defaultServerAddress = "http://localhost:8080";
+var defaultServerAddress = "https://2048-ai-4.cubeythecube.repl.co";
 
 var valueToRank = {
   0: '0', 2: '1', 4: '2', 8: '3', 16: '4', 32: '5', 64: '6', 128: '7', 256: '8',
